079ae983ab133b6913380e8e8a4989be8f4ebfc1
luisxmetalx <junitho_93@hotmail.com>
2016-01-14 02:30:03 -0500

    volumen y validacion de las matrices
    en matrices lo q hice es crear la funcion validar numero y ahora si ya
    solo me acepta digitos .... anexse  la parte del negro q como la verga
    no puede hacer un puto commit :v .....pilas revisen

--- Test1/Test1/Principal.cpp
+++ Test1/Test1/Principal.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <iostream>
 using namespace std;
+#define M_PI 3.14159265358979323846 /* pi */
 
 void mostrarMenuPrincipal();
 void menuPrincipal();
@@ -11,6 +12,7 @@ void imprimirP();
 void menuVolumenes(int opMenu1);
 void menuPolinomios(int opMenu1);
 void multiplicarMatrices();
+int validar_Numero(char numero[]);//valida si es digito ingrsando una cadena
 
 
 void main(){
@@ -83,30 +85,51 @@ void imprimirP(){
 
 void menuVolumenes(int opMenu1){
 	int opMenuVol = -1;
+	float radio = -1.0;
+	float altura = -1.0;
 	system("cls");
 	cout << "\n\n\n\t\t ----------M E N U  D E  O P C I O N E S--------";
 	cout << "\n\t\t|\t\t\t\t\t\t|";
 	cout << "\n\t\t|\t\t\t\t\t\t|";
-	cout << "\n\t\t|\t 1 > Volumen de una esfera\t\t\t|";
+	cout << "\n\t\t|\t 1 > Volumen de una esfera\t\t|";
 	cout << "\n\t\t|\t 2 > Volumen de un cilindro\t\t|";
 	cout << "\n\t\t|\t 3 > Otro volunen\t\t|";
 	cout << "\n\t\t|\t 4 <-  Atras\t\t\t\t|";
 	cout << "\n\t\t|\t\t\t\t\t\t|";
 	cout << "\n\t\t|\t\t\t\t\t\t|";
 	cout << "\n\t\t  ----------------------------------------------";
-	
+
 	do{
 		cout << "\n\n\t\t Ingrese una Opcion: ";
+		fflush(stdin);	cin >> opMenuVol;	fflush(stdin);
 	} while (opMenuVol<1 && opMenuVol>4);
-	fflush(stdin);	cin >> opMenuVol;	fflush(stdin);
-	
+
+
 	switch (opMenuVol){
 	case(1) :
-
+		do{
+			cout << "\n\n\t\t Ingrese el radio: ";
+			fflush(stdin);	cin >> radio;	fflush(stdin);
+		} while (radio<0);
+		cout << "\n\t\tEl volumen de la esfera de radio [r = " << radio << "] es: " << (4.0 / 3.0)*M_PI*pow(radio, 3) << " [u^3]";
+		cout << "\n\n\t\t[Presione una tecla para continuar]";
+		_getch();
 		opMenuVol = -1;
 		break;
 	case(2) :
+		do{
+			cout << "\n\n\t\t Ingrese el radio: ";
+			fflush(stdin);	cin >> radio;	fflush(stdin);
+		} while (radio<0);
 
+		do{
+			cout << "\n\n\t\t Ingrese la altura: ";
+			fflush(stdin);	cin >> altura;	fflush(stdin);
+		} while (altura<0);
+
+		cout << "\n\t\tEl volumen del cilindro de radio [r = " << radio << "] \n\t\t y altura [h=" << altura << "] es: " << altura*M_PI*pow(radio, 2) << " [u^3]";
+		cout << "\n\n\t\t[Presione una tecla para continuar]";
+		_getch();
 		opMenuVol = -1;
 		break;
 	case(3) :
@@ -115,12 +138,13 @@ void menuVolumenes(int opMenu1){
 		break;
 	case(4) :
 		opMenu1 = -1;
-//		opMenuVol = -1;
+
 		break;
 	default:
 		system("cls");			cout << "\nOpcion no valida. Intente de nuevo\n";	opMenuVol = -1;			break;
 	}
 }
+
 
 void menuPolinomios(int opMenu1){
 	int opMenuPol = -1;
@@ -167,20 +191,65 @@ void menuPolinomios(int opMenu1){
 void multiplicarMatrices(){
 	//cout << ":v \n"<<endl;
 	//_getch();
-	int m, n, f, c, i, j, k;
+	int m, n, f, c, i, j, k, op;
+	char dato[5];
 	float A[100][100], B[100][100], C[100][100];
+	op = 0;
 	cout<<"\n---------------------------------------------------"<<endl;
 	cout<<"\nINGRESE LA FILA Y COLUMNA DE LA PRIMERA MATRIZ"<<endl;
-	cout << "Fila: ";
-	cin>>m;
-	cout << "Columna: ";
-	cin>>n;
+	while (op != 1){
+		cout << "Fila: ";
+		cin >> dato;
+		if (validar_Numero(dato) == 0){
+			op = 1;
+		}
+		else{
+			cout << "Ingrase un dato numerico Por Favor. >:v " << endl;
+		}
+	}
+	m = atoi(dato);
+	//cout << "el numero de filas es : " << m << endl;
+	op = 0;
+	while (op != 1){
+		cout << "Columna: ";
+		cin >> dato;
+		if (validar_Numero(dato) == 0){
+			op = 1;
+		}
+		else{
+			cout << "Ingrase un dato numerico Por Favor. >:v " << endl;
+		}
+	}
+	n = atoi(dato);
+	op = 0;
+	
 	cout<<"\n---------------------------------------------------"<<endl;
 	cout<<"\nINGRESE LA FILA Y COLUMNA DE LA SEGUNDA MATRIZ"<<endl;
-	cout << "Fila: ";
-	cin>>f;
-	cout << "Columna: ";
-	cin>>c;
+	while (op != 1){
+		cout << "Fila: ";
+		cin >> dato;
+		if (validar_Numero(dato) == 0){
+			op = 1;
+		}
+		else{
+			cout << "Ingrase un dato numerico Por Favor. >:v " << endl;
+		}
+	}
+	f = atoi(dato);
+	//cout << "el numero de filas es : " << m << endl;
+	op = 0;
+	while (op != 1){
+		cout << "Columna: ";
+		cin >> dato;
+		if (validar_Numero(dato) == 0){
+			op = 1;
+		}
+		else{
+			cout << "Ingrase un dato numerico Por Favor. >:v " << endl;
+		}
+	}
+	c = atoi(dato);
+	op = 0;
 	cout<<endl;
 	if (n == f)
 	{
@@ -191,8 +260,18 @@ void multiplicarMatrices(){
 		for (i = 1; i <= m; i++)
 		for (j = 1; j <= n; j++)
 		{
-			cout<<"A("<<i<<","<<j<<")= ";
-			cin>>A[i][j];
+			while (op != 1){
+				cout << "A(" << i << "," << j << ")= ";
+				cin >> dato;
+				if (validar_Numero(dato) == 0){
+					op = 1;
+				}
+				else{
+					cout << "Ingrase un dato numerico Por Favor. >:v " << endl;
+				}
+			}
+			A[i][j]=atoi(dato);
+			op = 0;
 		}
 
 		cout<<endl;
@@ -202,8 +281,18 @@ void multiplicarMatrices(){
 		for (i = 1; i <= f; i++)
 		for (j = 1; j <= c; j++)
 		{
-			cout << "B(" << i << "," << j << ")= ";
-			cin >> B[i][j];
+			while (op != 1){
+				cout << "B(" << i << "," << j << ")= ";
+				cin >> dato;
+				if (validar_Numero(dato) == 0){
+					op = 1;
+				}
+				else{
+					cout << "Ingrase un dato numerico Por Favor. >:v " << endl;
+				}
+			}
+			B[i][j]=atoi(dato);
+			op = 0;
 		}
 		//OPERACION DE MULTIPLICACION
 		for (i = 1; i <= m; i++)
@@ -236,4 +325,21 @@ void multiplicarMatrices(){
 		cout<<"de la matriz B, !Vuelva a intentarlo!   "<<endl;
 	}
 	_getch();
+}
+
+int validar_Numero(char numero[]){
+	int i, sw,j;
+	i = 0;
+	sw = 0;
+	j = strlen(numero);
+	while (i < j && sw == 0){
+		if (isdigit(numero[i]) != 0){
+			i++;
+		}
+		else{
+			sw = 1;
+		}
+	}
+	return sw;
+
 }
